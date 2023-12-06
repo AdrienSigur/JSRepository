b55398d158257048fe474187c20eb186d985d38b
YakiGd <sigur.adrien@gmail.com>
2023-12-06 09:44:08 +0100

    boucle.js

---
+++ boucle.js
@@ -0,0 +1,146 @@
+/* Exo 2 
+const A = "A";
+const B = "B";
+
+for (let i = 0; i < 5; i++) {
+  console.log(A);
+  console.log(B);
+}
+*/
+//Exo 3
+/*
+const A = "A";
+const B = "B";
+
+for (let i = 0; i < 5; i++) {
+  console.log(A);
+  console.log(A);
+  console.log(A);
+  console.log(A);
+  console.log(B);
+}
+*/
+
+/*
+// Exo 4
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+*/
+
+// Exo 5
+/* 
+const int = 3;
+
+for (let i = 0; i < 12; i++) {
+  console.log(int + i);
+}
+
+*/
+/*
+// Exo 6
+
+let int = 1;
+
+let number = 0;
+
+for (let i = 0; i < 4; i++) {
+  console.log(number);
+  console.log(int);
+  console.log(int * 2);
+  console.log(int * 3);
+}
+*/
+/*
+
+// Exo 8
+let x = 100;
+
+let y = 1;
+
+console.log(x);
+
+for (let i = 0; i < 3; i++) {
+
+  console.log(y);
+  console.log(y * 2);
+  console.log((x += y * 3));
+}
+*/
+
+// Exo 9
+/*
+let x = 100;
+
+let y = 0;
+
+console.log(y);
+
+for (let i = 0; i < 5; i++) {
+  console.log(x + y);
+
+  console.log(x * 2 + y * 2);
+}
+*/
+/*
+// Exo 10
+
+for (let d1 = 1; d1 < 7; d1++) {
+  for (let d2 = 1; d2 < 7; d2++) {
+    console.log(d1, d2);
+  }
+}
+*/
+/*
+// Exo 13
+
+let nombre = 1;
+let limite = 9;
+
+for (let i = 1; i <= limite; i++) {
+  let resultat = nombre * i;
+
+  console.log(nombre + " * " + i + " = " + resultat);
+}
+*/
+/*
+// Exo 14
+
+let nombre = 2;
+let limite = 9;
+
+for (let i = 1; i <= limite; i++) {
+  let resultat = nombre * i;
+
+  console.log(nombre + " * " + i + " = " + resultat);
+}
+*/
+/*
+//Exo 15
+
+let nombre = 2;
+let limite = 9;
+
+
+for (let j = 1; j < 9; j++) {
+  for (let i = 1; i <= limite; i++) {
+    let resultat = nombre * i;
+
+    console.log(nombre + " * " + i + " = " + resultat);
+  }
+  nombre += 1;
+}
+*/
+/*
+//Exo 16
+
+let somme = 1;
+// Loop repeat 100 times  start var I = 2 
+for (i = 2; i <=101; i++) {
+  somme = i + somme;
+}
+
+console.log("La somme est de " + somme);
+*/
